b17af65066dc6d710e748e009cfa9449f1e99e92
mikalfoster <mikalfoster36@gmail.com>
2023-02-23 13:43:16 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks () {
+function distanceFromHqInBlocks(number) {
     //number of blocks from pickup to HQ (42)
-    const HQ = '42 blocks';
-    const pickup = '43 blocks';
+    const destination = 42
+    let distance = Math.abs(number - destination)
+    return distance;
 }
-console.log (distanceFromHqInBlocks());
-//function distanceFromHqInFeet () {
+
+function distanceFromHqInFeet(feet) {
     //call distanceFromHqInBlocks function
     //pass the distance..Feet to distance..Blocks
     //return the value and figure out the feet
-    //distanceFromHqInBlocks();
-//}
-//function distanceTravelledInFeet () {
-    //one block equals 264 feet
-//}
-//function calculatesFarePrice ('length', 'freeDistance') {
-    //if (length <= 400) {
-    //  return gives customer a free ride
-    //}
+    const distanceFeet = distanceFromHqInBlocks(feet) * 264
+    return distanceFeet
+}
+function distanceTravelledInFeet (start, end) {
+    const blocks = Math.abs(start - end)*264
+    return blocks
+}
+function calculatesFarePrice (start, destination) {
+    const feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+        return 0
+    } else if (feet > 400 && feet < 2000) {
+        return (feet - 400) * .02
+    } else if (feet >= 2000 && feet <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+ }
+    // if (feet <= 400) {
+    //   return gives customer a free ride
+    // }
     //if (length <= 2000 - 400) {
     //  return times the answer by .02
     //}
