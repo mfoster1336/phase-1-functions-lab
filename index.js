4b595600b5f91250dd7cd352539c460987baac50
mikalfoster <mikalfoster36@gmail.com>
2023-02-22 18:24:29 -0600

    Functions Lab First

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks () {
+    //number of blocks from pickup to HQ (42)
+    const HQ = '42 blocks';
+    const pickup = '43 blocks';
+}
+console.log (distanceFromHqInBlocks());
+//function distanceFromHqInFeet () {
+    //call distanceFromHqInBlocks function
+    //pass the distance..Feet to distance..Blocks
+    //return the value and figure out the feet
+    //distanceFromHqInBlocks();
+//}
+//function distanceTravelledInFeet () {
+    //one block equals 264 feet
+//}
+//function calculatesFarePrice ('length', 'freeDistance') {
+    //if (length <= 400) {
+    //  return gives customer a free ride
+    //}
+    //if (length <= 2000 - 400) {
+    //  return times the answer by .02
+    //}
+    //if (length =< 2000 - 400) {
+    //  return $25 as a flat rate    
+    //}
+    // if (length >= 2500) {
+    //  return "cannot travel that far"
+    //}
+//}
